261cd38076dece51f5983bf6b56f483d755550ad
juandiegoromero <ingjuandiegoromero@gmail.com>
2026-06-03 18:43:12 -0500

    Comment out geopy imports in app.py
    Comment out unused geopy imports.

--- app.py
+++ app.py
@@ -3,10 +3,10 @@ import pandas as pd
 import libreria_funciones as lf
 import plotly.express as px
 import plotly.graph_objects as go
-import geopy from geopy.geocoders 
-import Nominatim
-import geopy from geopy.extra.rate_limiter 
-import RateLimiter 
+# import geopy from geopy.geocoders 
+# import Nominatim
+# import geopy from geopy.extra.rate_limiter 
+# import RateLimiter 
 
 
 st.image("python.png")
