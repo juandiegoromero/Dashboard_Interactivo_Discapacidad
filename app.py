bcc1162962e7119e22540e0027b813b1f7a4405f
juandiegoromero <ingjuandiegoromero@gmail.com>
2026-06-04 17:09:36 -0500

    Update app.py

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ st.image("Includata EC.png")
 st.title("Dashboard Interactivo para el Monitoreo de la Discapacidad en el Sistema Educativo Ecuatoriano")
 
 st.sidebar.makdown(
-  """
-    ## Ojetivo de la aplicación:
+    """
+    ##*Ojetivo de la aplicación:*
     Desarrollar un dashboard interactivo para el monitoreo de estudiantes con discapacidad en el sistema educativo ecuatoriano mediante técnicas de Ciencia de Datos y la plataforma Streamlit.br>
     """,
     unsafe_allow_html=True
