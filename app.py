22dff97cbdd05a8e43021b2ca6c0dd7d94817f29
juandiegoromero <ingjuandiegoromero@gmail.com>
2026-06-04 18:07:48 -0500

    Update banner image in app.py

--- app.py
+++ app.py
@@ -289,4 +289,4 @@ df_proyeccion = pd.DataFrame({
 st.sidebar.subheader("Proyección de Estudiantes con Discapacidad")
 st.sidebar.dataframe(df_proyeccion, use_container_width=True)
 
-st.image("Banner_IncludataEC.png")
+st.image("Banner_IncludataEC2.png")
