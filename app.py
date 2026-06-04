8ad374f208b1fd245f8f9df9a663e28aebf7eaf3
juandiegoromero <ingjuandiegoromero@gmail.com>
2026-06-04 17:34:14 -0500

    Add image banner to sidebar

--- app.py
+++ app.py
@@ -290,5 +290,7 @@ df_proyeccion = pd.DataFrame({
 st.sidebar.subheader("Proyección de Estudiantes con Discapacidad")
 st.sidebar.dataframe(df_proyeccion, use_container_width=True)
 
+st.image("banner_includata EC.png", use_container_width=True)
+
 
 st.image("banner_includata.png", use_container_width=True)
