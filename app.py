1445fd59b24b3c188732b57e77493e83209f3187
juandiegoromero <ingjuandiegoromero@gmail.com>
2026-06-04 17:15:26 -0500

    Fix sidebar markdown formatting in app.py

--- app.py
+++ app.py
@@ -10,16 +10,15 @@ st.image("Includata EC.png")
 
 st.title("Dashboard Interactivo para el Monitoreo de la Discapacidad en el Sistema Educativo Ecuatoriano")
 
-st.sidebar.markdown(
-    """
+st.sidebar.markdown(f """
     ##**Ojetivo de la aplicación:**
     Desarrollar un dashboard interactivo para el monitoreo de estudiantes con discapacidad en el sistema educativo ecuatoriano mediante técnicas de Ciencia de Datos y la plataforma Streamlit.<br>
     """,
     unsafe_allow_html=True
 )
 
-st.sidebar.markdown(f("""
-    ## Integrantes:
+st.sidebar.markdown(f """
+    ## **Integrantes:**
     Luis Alberto Chicaiza González<br>
     Juan Diego Romero Fernández
     """,
