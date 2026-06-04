96b2319e40af309554d79c9310880ff03d279abf
juandiegoromero <ingjuandiegoromero@gmail.com>
2026-06-04 17:06:18 -0500

    Fix sidebar objective formatting in app.py

--- app.py
+++ app.py
@@ -10,8 +10,13 @@ st.image("Includata EC.png")
 
 st.title("Dashboard Interactivo para el Monitoreo de la Discapacidad en el Sistema Educativo Ecuatoriano")
 
-st.sidebar.title("**Ojetivo de la aplicación**:")
-st.sidebar.subheader("Desarrollar un dashboard interactivo para el monitoreo de estudiantes con discapacidad en el sistema educativo ecuatoriano mediante técnicas de Ciencia de Datos y la plataforma Streamlit.")
+st.sidebar.makdown(
+  """
+    ## Ojetivo de la aplicación:
+    Desarrollar un dashboard interactivo para el monitoreo de estudiantes con discapacidad en el sistema educativo ecuatoriano mediante técnicas de Ciencia de Datos y la plataforma Streamlit.br>
+    """,
+    unsafe_allow_html=True
+)
 
 st.sidebar.markdown(
     """
