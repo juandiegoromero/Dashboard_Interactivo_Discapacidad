6afe26b4a10450d36b23dce18c749b644b731944
juandiegoromero <ingjuandiegoromero@gmail.com>
2026-06-04 16:58:34 -0500

    Enhance sidebar title with detailed application objective

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ st.image("Includata EC.png")
 
 st.title("Dashboard Interactivo para el Monitoreo de la Discapacidad en el Sistema Educativo Ecuatoriano")
 
-st.sidebar.title("Ojetivo de la aplicación:")
+st.sidebar.title("Ojetivo de la aplicación: Desarrollar un dashboard interactivo para el monitoreo de estudiantes con discapacidad en el sistema educativo ecuatoriano mediante técnicas de Ciencia de Datos y la plataforma Streamlit.")
 
 st.sidebar.markdown(
     """
