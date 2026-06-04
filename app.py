7f5b3b94c1b1ebc352697af61aa9d630f4342757
juandiegoromero <ingjuandiegoromero@gmail.com>
2026-06-04 18:20:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -292,5 +292,5 @@ df_proyeccion = pd.DataFrame({
 st.sidebar.subheader("Proyección de Estudiantes con Discapacidad")
 st.sidebar.dataframe(df_proyeccion, use_container_width=True)
 
-st.footer.image("Banner_IncludataEC2.png")
+st.image("Banner_IncludataEC2.png", use_container_width=True)
 
