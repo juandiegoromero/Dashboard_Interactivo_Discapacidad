7aba82a1a76f87b8356b6948bbe218bb4d591139
juandiegoromero <ingjuandiegoromero@gmail.com>
2026-06-04 17:54:40 -0500

    Update app.py

--- app.py
+++ app.py
@@ -289,4 +289,7 @@ df_proyeccion = pd.DataFrame({
 st.sidebar.subheader("Proyección de Estudiantes con Discapacidad")
 st.sidebar.dataframe(df_proyeccion, use_container_width=True)
 
-st.image("assets/banner_includata.png", use_container_width=True)
+st.image(
+    "https://github.com/juandiegoromero/Dashboard_Interactivo_Discapacidad/edit/main/Banner_IncludataEC.png",
+    use_container_width=True
+)
